2f5e6c1e90b02cdb164ac24c6951e8e2ee2d9eaa
sshong1991 <sshong1991@gmail.com>
2019-01-06 15:38:28 +0900

    Init Todo project task app
    Working todo model

--- todo/task/models.py
+++ todo/task/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+PRIORITY = (
+    ('1', '중요'),
+    ('2', '보통'),
+    ('3', '사소'),
+)
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    note = models.TextField(blank=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=1, choices=PRIORITY)
+
+    created_date = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["priority"]

b8846d7b9be66694062f0a67372538290924f2d7
sshong1991 <sshong1991@gmail.com>
2019-01-15 21:44:29 +0900

    Edit task app form

--- todo/task/forms.py
+++ todo/task/forms.py
@@ -1,22 +1,58 @@
 from django import forms
-from .models import Todo
+from .models import Todo, PRIORITY
+
 
 class TodoForm(forms.ModelForm):
-
     class Meta:
         model = Todo
-        fields = ['title', 'note', 'deadline', 'completed', 'priority']
-
+        fields = ['title', 'note', 'deadline', 'priority']
+        # fields = '__all__'
+        # widgets = {
+        #     'deadline': DatePickerWidget,
+        # }
 
     title = forms.CharField(
-        label='title',
+        label='할 일',
         widget=forms.TextInput(
             attrs={
-                'class': '',
-                'placeholder': 'Enter the title',
+                'class': 'form-control',
+                'placeholder': '무엇을 할까',
                 'id': 'title',
             }
         )
     )
 
+    note = forms.CharField(
+        label='자세한 내용',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '자세한 내용',
+            }
+        )
+    )
 
+    deadline = forms.DateTimeField(
+        label='언제까지',
+        widget=forms.DateTimeInput(
+            attrs={
+                'class': 'form-control',
+            }
+        )
+    )
+
+    priority = forms.ChoiceField(
+        choices=PRIORITY,
+        label="중요도",
+        widget=forms.Select(
+            attrs={
+                'class': 'form-control',
+            }
+        )
+    )
+
+
+# class TodoCompleteForm(forms.ModelForm):
+#     class Meta:
+#         model = Todo
+#         fields = ['completed']
